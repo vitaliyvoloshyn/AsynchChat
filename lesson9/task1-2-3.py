2d3cd202c7a1ecdb8cb44426de7ccce8f0215a2c
vitaliyvoloshyn <vitaliy.srt1945@gmail.com>
2022-10-17 15:01:43 +0300

    lesson9 done Проверка доступности хостов по списку, генерируемым с помощью ip_address

--- lesson9/task1-2-3.py
+++ lesson9/task1-2-3.py
@@ -1,7 +1,13 @@
 import pythonping
+from ipaddress import ip_address
 
 
-ip_list = ['192.168.1.102', '192.168.1.103', '192.168.1.104', '192.168.1.105']
+def host_gen(count: int = 1, base_ip: str = '192.168.1.100') -> list[str]:
+    """
+    Генерирует список ip адресов путем арифметического добавления 1 к последнему октету базового адреса
+    """
+    base_ip = ip_address(base_ip)
+    return [str(base_ip + i) for i in range(count)]
 
 
 def ping(ips: list[str]) -> None:
@@ -11,10 +17,17 @@ def ping(ips: list[str]) -> None:
         print(ip + ' - reachable' if ping_resp else ip + ' - unreachable')
 
 
+ip_list = host_gen(10)
 ping(ip_list)
 
 # вывод:
+# 192.168.1.100 - unreachable
+# 192.168.1.101 - reachable
 # 192.168.1.102 - reachable
-# 192.168.1.103 - reachable
+# 192.168.1.103 - unreachable
 # 192.168.1.104 - unreachable
 # 192.168.1.105 - reachable
+# 192.168.1.106 - unreachable
+# 192.168.1.107 - unreachable
+# 192.168.1.108 - unreachable
+# 192.168.1.109 - unreachable
